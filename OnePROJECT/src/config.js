d3af0cfcb27c319ab801998df7e2baaa0809ab71
Singh <a698219@cis.neustar.com>
2019-07-30 12:32:18 +0530

    disabled https configuration

--- OnePROJECT/src/config.js
+++ OnePROJECT/src/config.js
@@ -1,4 +1,11 @@
 export const config = {
   APP_NAME: 'OnePROJECT',
-  SERVER_URL: 'https://localhost:8001/'
+  /**
+   * Enable below line for HTTP
+   */
+  SERVER_URL: 'http://localhost:8001/'
+  /*
+   * Enable below lines for HTTPS
+   */
+  // SERVER_URL: 'https://localhost:8001/'
 }
